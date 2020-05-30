a312a1bcbfedaf3371fcdf15978ece6ad1d7ed62
Aftab Alam <532906+one-aalam@users.noreply.github.com>
2020-05-30 11:37:35 +0530

    feat: Add Express for APIs

--- packages/micro-three/index.js
+++ packages/micro-three/index.js
@@ -1,4 +1,23 @@
-function sum(a, b) {
-    return a + b;
-  }
-module.exports = sum;
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get('/', async (req, res) => {
+    res.end('Welcome to Micro #3')
+});
+
+app.post('/', (req, res) => {
+  const { value = 0 } = req.body;
+  return res.json({
+    value: Number(value) + 1
+  });
+});
+
+
+app.listen(process.env.PORT || 3002, () => {
+  console.log('Server listening on PORT', process.env.PORT || 3002);
+});
+
+module.exports = app;
